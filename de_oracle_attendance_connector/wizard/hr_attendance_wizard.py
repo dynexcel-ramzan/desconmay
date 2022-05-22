2a610a9197b1000c6422127ebcb84af721290810
dynexcel-ramzan <77322048+dynexcel-ramzan@users.noreply.github.com>
2022-05-22 13:16:07 +0500

    update att

--- de_oracle_attendance_connector/wizard/hr_attendance_wizard.py
+++ de_oracle_attendance_connector/wizard/hr_attendance_wizard.py
@@ -4,6 +4,8 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError, UserError
 from datetime import date, datetime, timedelta
 
+
+
 _logger = logging.getLogger(__name__)
 
 
